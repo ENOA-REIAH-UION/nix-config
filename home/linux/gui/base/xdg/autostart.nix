7c8f02bc91e480c473a03baf75a0f2fb7080da42
ENOA-ANSUL-EDEN <i@enoa.me>
2026-02-21 16:26:42 +0800

    feat: 调整开机自启程序

--- home/linux/gui/base/xdg/autostart.nix
+++ home/linux/gui/base/xdg/autostart.nix
@@ -4,20 +4,21 @@
   xdg.autostart.enable = true;
   # This fixes nixpak sandboxed apps (like firefox) accessing mapped folders correctly
   xdg.autostart.entries = [
-    "${pkgs.foot}/share/applications/foot.desktop"
-    "${pkgs.alacritty}/share/applications/Alacritty.desktop"
-    "${pkgs.ghostty}/share/applications/com.mitchellh.ghostty.desktop"
+    # "${pkgs.foot}/share/applications/foot.desktop"
+    # "${pkgs.alacritty}/share/applications/Alacritty.desktop"
+    # "${pkgs.ghostty}/share/applications/com.mitchellh.ghostty.desktop"
 
     "${pkgs.clash-verge-rev}/share/applications/clash-verge.desktop"
 
     # nixpaks
     "${pkgs.nixpaks.firefox}/share/applications/org.mozilla.firefox.desktop"
-    "${pkgs.nixpaks.telegram-desktop}/share/applications/org.telegram.desktop.desktop"
-  ]
-  ++ (
-    if pkgs.stdenv.isx86_64 then
-      [ "${pkgs.google-chrome}/share/applications/google-chrome.desktop" ]
-    else
-      [ "${pkgs.chromium}/share/applications/chromium-browser.desktop" ]
-  );
+    "${pkgs.nixpaks.qq}/share/applications/com.qq.QQ.desktop"
+    # "${pkgs.nixpaks.telegram-desktop}/share/applications/org.telegram.desktop.desktop"
+  ];
+  # ++ (
+  #   if pkgs.stdenv.isx86_64 then
+  #     [ "${pkgs.google-chrome}/share/applications/google-chrome.desktop" ]
+  #   else
+  #     [ "${pkgs.chromium}/share/applications/chromium-browser.desktop" ]
+  # );
 }
